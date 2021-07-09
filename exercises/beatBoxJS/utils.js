1c63edf98372f14c2e90609f06f697402282865a
Shiyam <shiyams315@gmail.com>
2021-07-09 19:53:07 +0530

    Responsive BetBox

--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -1,35 +1,56 @@
 /**
  * Beat class that keeps track of playing the audio
  * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
- * HINT: Create a play function to play the audio if called 
+ * HINT: Create a play function to play the audio if called
  */
 class Beat {
+  constructor(audioSrc) {
+    this.audioSrc = new Audio(audioSrc);
+  }
+
+  play = () => {
+    this.audioSrc.currentTime = 0;
+    this.audioSrc.play();
+  };
 }
-
-
 
 /**
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-    constructor(color, keyCode){
-    }
+  constructor(color, keyCode) {
+    this.color = color;
+    this.keyCode = keyCode;
+    this.element = document.getElementById(keyCode);
+    this.setButtonColorInHTML();
+    this.setATransistionEndListner();
+  }
 
-    /**
-     * Set the button color based on color specified
-     */
-    setButtonColorInHTML = () => {
-    }
+  setATransistionEndListner = () => {
+    this.element.addEventListener("transitionend", () => {
+      this.deselect();
+    });
+  };
+  /**
+   * Set the button color based on color specified
+   */
+  setButtonColorInHTML = () => {
+    this.element.style.borderColor = this.color;
+  };
 
-    /**
-     * Select function to set the background color and boxShadow
-     */
-    select = () => {
-    }
+  /**
+   * Select function to set the background color and boxShadow
+   */
+  select = () => {
+    this.element.style.backgroundColor = this.color;
+    this.element.style.boxShadow = `0 0 17px 0 ${this.color}`;
+  };
 
-    /**
-     * Deselect function to reset background color and boxShadow
-     */
-    deselect = () => {
-    }
+  /**
+   * Deselect function to reset background color and boxShadow
+   */
+  deselect = () => {
+    this.element.style.backgroundColor = "transparent";
+    this.element.style.boxShadow = "none";
+  };
 }
